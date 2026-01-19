1bb921cd22439f954df9387e7dd7ba0e65b669d3
JRC <69986863+JRGCr@users.noreply.github.com>
2026-01-18 22:06:42 -0500

    Add site/config.js

---
+++ site/config.js
@@ -0,0 +1,11 @@
+window.WAITLIST_CONFIG = {
+  "headline": "adsfafafdasdfasdfadfads",
+  "description": "sfasdfasdfdfafasdfsaf",
+  "collect_name": true,
+  "collect_company": true,
+  "primary_color": "#3b82f6",
+  "logo_url": "",
+  "turnstile_enabled": false,
+  "turnstile_site_key": "",
+  "api_url": "WORKER_URL_PLACEHOLDER"
+};
